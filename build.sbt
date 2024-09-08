d28ca58149f9e2502007706cbfae236c34179df8
Sparkles-Laurel <info@yigitovski.com>
2024-09-08 10:19:49 +0300

    initial commit

---
+++ build.sbt
@@ -0,0 +1,3 @@
+name := "gay.rainbowlinux.infra.cd.lefty"
+version := "0.1.0"
+scalaVersion := "2.13.14"
---
+++ src/main/scala/Lefty.scala
@@ -0,0 +1,7 @@
+// Lefty.scala - Entry point for Lefty
+
+package gay.rainbowlinux.infra.cd.lefty
+
+object Lefty extends App {
+    println("Hello, world!")
+}
