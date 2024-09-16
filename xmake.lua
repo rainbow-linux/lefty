917c9353291481a8e3c789fb1b3a07b033908dcb
Sparkles-Laurel <info@yigitovski.com>
2024-09-16 22:34:03 +0300

    fix image name in xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -7,7 +7,7 @@ set_version("1.0")
 -- Add a custom target to build the Docker image
 task("docker")
     on_run(function (target)
-        local image_name = "my-scala-app"
+        local image_name = "lefty"
         local tag = "latest"
         os.exec("docker build -t %s:%s .", image_name, tag)
     end)
